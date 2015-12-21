1b4bb989c36ed42c5e759a7e7ff6cff4e768127f
yamabit <yama@supership.jp>
2015-12-21 10:19:33 +0900

    list

--- RxPlayground/app/src/main/java/com/yyaammaa/rxplayground/RetroActivity.java
+++ RxPlayground/app/src/main/java/com/yyaammaa/rxplayground/RetroActivity.java
@@ -10,11 +10,14 @@ import com.yyaammaa.rxplayground.retrofit.GitHub;
 import com.yyaammaa.rxplayground.retrofit.GitHubApiClient;
 import com.yyaammaa.rxplayground.util.Logr;
 
+import java.util.List;
+
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import rx.Observable;
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 public class RetroActivity extends ActionBarActivity {
@@ -35,11 +38,59 @@ public class RetroActivity extends ActionBarActivity {
 
   @OnClick(R.id.act_retro_test_button_1)
   void onTest1Click() {
-    get1();
+    //get1();
+    get2();
   }
 
   private void test() {
 
+  }
+
+  private void get2() {
+    GitHubApiClient client = GitHub.createApiClient();
+    Observable<List<Gist>> obs = client.getGistsByUsername("yamabit");
+    obs.subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .flatMap(new Func1<List<Gist>, Observable<Gist>>() {
+          @Override
+          public Observable<Gist> call(List<Gist> gists) {
+            return Observable.from(gists);
+          }
+        })
+        .subscribe(new Observer<Gist>() {
+          @Override
+          public void onCompleted() {
+            Logr.e("onCompleted");
+          }
+
+          @Override
+          public void onError(Throwable throwable) {
+            Logr.e("onError");
+            throwable.printStackTrace();
+          }
+
+          @Override
+          public void onNext(Gist gist) {
+            Logr.e(gist.url);
+          }
+        });
+//        .subscribe(new Observer<List<Gist>>() {
+//          @Override
+//          public void onCompleted() {
+//            Logr.e("onCompleted");
+//          }
+//
+//          @Override
+//          public void onError(Throwable throwable) {
+//            Logr.e("onError");
+//            throwable.printStackTrace();
+//          }
+//
+//          @Override
+//          public void onNext(List<Gist> gists) {
+//            Logr.e("size = " + gists.size());
+//          }
+//        });
   }
 
   private void get1() {
--- RxPlayground/app/src/main/java/com/yyaammaa/rxplayground/retrofit/GitHubApiClient.java
+++ RxPlayground/app/src/main/java/com/yyaammaa/rxplayground/retrofit/GitHubApiClient.java
@@ -1,12 +1,16 @@
 package com.yyaammaa.rxplayground.retrofit;
 
+import java.util.List;
+
 import retrofit.http.GET;
 import retrofit.http.Path;
 import rx.Observable;
 
 public interface GitHubApiClient {
 
-  // e.g. https://api.github.com/users/:username/gists
+  // e.g. https://api.github.com/users/yamabit/gists
+  @GET("users/{user}/gists")
+  Observable<List<Gist>> getGistsByUsername(@Path("user") String username);
 
   // e.g. https://api.github.com/gists/f36f5dba0c2ae784688c
   @GET("gists/{id}")
