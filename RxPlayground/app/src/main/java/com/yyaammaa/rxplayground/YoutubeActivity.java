5a15bc82d4ba3d5143165fa04a2ac321a621ba89
yamabit <yama@supership.jp>
2016-01-07 18:12:38 +0900

    2

--- RxPlayground/app/src/main/java/com/yyaammaa/rxplayground/YoutubeActivity.java
+++ RxPlayground/app/src/main/java/com/yyaammaa/rxplayground/YoutubeActivity.java
@@ -3,11 +3,13 @@ package com.yyaammaa.rxplayground;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
+import com.yyaammaa.rxplayground.util.Dummy;
 import com.yyaammaa.rxplayground.util.Logr;
 
 import butterknife.Bind;
@@ -15,10 +17,14 @@ import butterknife.BindString;
 import butterknife.ButterKnife;
 import rx.subscriptions.CompositeSubscription;
 
+// memo: https://developers.google.com/youtube/android/player/reference/com/google/android/youtube/player/YouTubeThumbnailView.OnInitializedListener?hl=ja
+
 public class YoutubeActivity extends YouTubeBaseActivity {
 
   @BindString(R.string.youtube_api_key) String mApiKey;
   @Bind(R.id.act_youtube_playerview) YouTubePlayerView mPlayerView;
+  @Bind(R.id.act_youtube_playerview2) YouTubePlayerView mPlayerView2;
+  @Bind(R.id.act_youtube_text) TextView mTextView;
 
   private YoutubeActivity self = this;
 
@@ -46,6 +52,8 @@ public class YoutubeActivity extends YouTubeBaseActivity {
   }
 
   private void setUpViews() {
+
+    mTextView.setText(Dummy.text());
 
     // 例によってGenymotionではダメ
     mPlayerView.initialize(mApiKey, new YouTubePlayer.OnInitializedListener() {
@@ -124,6 +132,84 @@ public class YoutubeActivity extends YouTubeBaseActivity {
         Logr.e("onInitializationFailure: " + error.toString());
       }
     });
+
+    // 2つはダメぽい
+//    mPlayerView2.initialize(mApiKey, new YouTubePlayer.OnInitializedListener() {
+//      @Override
+//      public void onInitializationSuccess(YouTubePlayer.Provider provider,
+//                                          YouTubePlayer player,
+//                                          boolean wasRestored) {
+//        Logr.e("onInitializationSuccess");
+//
+//        player.setPlaybackEventListener(new YouTubePlayer.PlaybackEventListener() {
+//          @Override
+//          public void onPlaying() {
+//            Logr.e("onPlaying");
+//          }
+//
+//          @Override
+//          public void onPaused() {
+//            Logr.e("onPaused");
+//          }
+//
+//          @Override
+//          public void onStopped() {
+//            Logr.e("onStopped");
+//          }
+//
+//          @Override
+//          public void onBuffering(boolean b) {
+//            Logr.e("onBuffering");
+//          }
+//
+//          @Override
+//          public void onSeekTo(int i) {
+//            Logr.e("onSeekTo");
+//          }
+//        });
+//
+//        player.setPlayerStateChangeListener(new YouTubePlayer.PlayerStateChangeListener() {
+//          @Override
+//          public void onLoading() {
+//            Logr.e("onLoading");
+//          }
+//
+//          @Override
+//          public void onLoaded(String s) {
+//            Logr.e("onLoaded");
+//          }
+//
+//          @Override
+//          public void onAdStarted() {
+//            Logr.e("onAdStarted");
+//          }
+//
+//          @Override
+//          public void onVideoStarted() {
+//            Logr.e("onVideoStarted");
+//          }
+//
+//          @Override
+//          public void onVideoEnded() {
+//            Logr.e("onVideoEnded");
+//          }
+//
+//          @Override
+//          public void onError(YouTubePlayer.ErrorReason errorReason) {
+//            Logr.e("onError");
+//          }
+//        });
+//
+//        //player.loadVideo("t-e4YJ-3Cb0");
+//        player.cueVideo("t-e4YJ-3Cb0");
+//      }
+//
+//      @Override
+//      public void onInitializationFailure(YouTubePlayer.Provider provider,
+//                                          YouTubeInitializationResult error) {
+//        Logr.e("onInitializationFailure: " + error.toString());
+//      }
+//    });
 
   }
 
